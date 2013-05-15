8232138d38259b87daa257f84b0ab4ee50d09c5e
Mark Bell <mbell697@gmail.com>
2013-05-15 19:44:51 -0400

    Add a non-failing example

--- src/main/java/org/ebean/test/enhance/TestEntity2.java
+++ src/main/java/org/ebean/test/enhance/TestEntity2.java
@@ -5,19 +5,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-public class TestEntity {
-  //This will trigger a ClassNotFoundException
+public class TestEntity2 {
+  //This will NOT trigger a ClassNotFoundException
   public List<TestNonEntity> test() {
     List<TestNonEntity> result = new ArrayList<TestNonEntity>();
     TestNonEntity match = null;
     for (TestNonEntity t: result) {
 
     }
-    if (match == null) {
 
-    } else {
-        TestNonEntity pli = new TestNonEntity();
-    }
 
     return result;
   }
